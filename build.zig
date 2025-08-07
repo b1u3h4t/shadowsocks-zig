151041d5bb437a8a901c48b240cd13aa60b23950
root <root@static.7.187.203.116.clients.your-server.de>
2025-08-07 12:47:18 +0800

    ReleaseFast

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardOptimizeOption(.{});
+    const mode = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
 
     const exe = b.addExecutable(.{
         .name = "shadowsocks-zig",
